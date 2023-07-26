b3ea0ec1d2b4ad59b0e6b74a2b7f7c5413fd4f2a
Ben Halperin <bahalperin@gmail.com>
2023-07-25 20:53:32 -0600

    add channel create test

--- lib/ash_chat/chat/chat.ex
+++ lib/ash_chat/chat/chat.ex
@@ -4,4 +4,9 @@ defmodule App.Chat do
   resources do
     registry App.Chat.Registry
   end
+
+  authorization do
+    require_actor? true
+    authorize :by_default
+  end
 end
---
+++ test/app/chat_test.exs
@@ -0,0 +1,26 @@
+defmodule App.ChatTest do
+  use App.DataCase
+
+  alias App.Chat
+
+  describe "channel" do
+    alias App.Chat.Channel
+
+    @invalid_attrs %{name: 47}
+    @actor %{ id: "uuid"}
+
+    test "create with valid data and actor creates channel" do
+      assert {:ok, %Channel{} = channel} = Channel.create(%{ name: "main" }, actor: @actor)
+    end
+
+    test "create without an actor fails" do
+      assert_raise Ash.Error.Forbidden.ApiRequiresActor, fn ->
+        Channel.create(%{ name: "main" })
+      end
+    end
+
+    test "create fails with invalid data" do
+      assert {:error, _} = Channel.create(@invalid_attrs, actor: @actor)
+    end
+  end
+end
