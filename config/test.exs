b00c6143dbdcb8204af7fcf2d0c03fdc5ade659b
Ben Halperin <bahalperin@gmail.com>
2023-07-25 21:25:25 -0600

    fix test db username

--- config/test.exs
+++ config/test.exs
@@ -6,7 +6,7 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :ash_chat, App.Repo,
-  username: "benhalperin",
+  username: "postgres",
   password: "postgres",
   hostname: "localhost",
   database: "ash_chat_test#{System.get_env("MIX_TEST_PARTITION")}",
